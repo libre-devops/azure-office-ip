db4737ad29ae605517c1a1e0ab6d2ff640ff2fcd
Craig Thacker <craigthackerx@gmail.com>
2022-05-07 16:07:30 +0100

    add terraform

--- get-azure-endpoints-minimal.py
+++ get-azure-endpoints-minimal.py
@@ -10,6 +10,7 @@ def clear():
     ip_list = None
 
 
+# Lists all Azure regions and prints the list when called
 def get_azure_endpoints():
     """
     Get Azure Datacenter endpoints IP addresses
@@ -23,6 +24,21 @@ def get_azure_endpoints():
     print(sorted_ip_list)
 
 
+
+# Lists all office365 object
+def get_o365_endpoints_json():
+    sorted_ip_list = {}
+    request_id = uuid.uuid4()
+    '''
+    Get Office 365 endpoints IP addresses
+    '''
+    clear()
+    office_response = requests.get(
+        "https://endpoints.office.com/endpoints/worldwide?clientrequestid={}".format(request_id))
+    object_list = office_response.json()
+    pprint.pprint(object_list)
+
+# Lists all office365 endpoint IPs and prints in a pretty json format
 def get_o365_endpoints_ips():
     sorted_ip_list = {}
     request_id = uuid.uuid4()
@@ -33,12 +49,9 @@ def get_o365_endpoints_ips():
     office_response = requests.get(
         "https://endpoints.office.com/endpoints/worldwide?clientrequestid={}".format(request_id))
     ip_list = office_response.json()
-    for item in ip_list:
-        if item.get("ips") is not None:
-            ip_list = item["ips"]
     pprint.pprint(ip_list)
 
-
+# Lists all office365 endpoint urls and prints in a pretty json format
 def get_o365_endpoints_urls():
     sorted_ip_list = {}
     request_id = uuid.uuid4()
@@ -48,8 +61,11 @@ def get_o365_endpoints_urls():
     clear()
     office_response = requests.get(
         "https://endpoints.office.com/endpoints/worldwide?clientrequestid={}".format(request_id))
-    url_list = office_response.json()
-    pprint.pprint(url_list)
+    urls_list = office_response.json()
+    for item in urls_list:
+        if item.get("ips") is not None:
+            urls_list = item["urls"]
+    pprint.pprint(urls_list)
 
 
-get_o365_endpoints_ips()
+get_o365_endpoints_json()
