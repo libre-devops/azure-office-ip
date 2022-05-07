501b0bbe3a69687d0df8eb715d07400ad3bc8056
Craig Thacker <craigthackerx@gmail.com>
2022-05-07 20:19:39 +0100

    update

--- terraform/build.tf
+++ terraform/build.tf
@@ -111,12 +111,12 @@ module "fnc_app" {
   }
 }
 
-resource "azurerm_storage_container" "web_blob" {
+resource "azurerm_storage_container" "web_blob_container" {
   name                 = "$web"
   storage_account_name = module.sa.sa_name
 }
 
-resource "azurerm_storage_container" "web_blob" {
+resource "azurerm_storage_container" "urls_blob_container" {
   name                 = "$urls"
   storage_account_name = module.sa.sa_name
 }
